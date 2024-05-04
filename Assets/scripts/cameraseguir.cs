4fdea96467b1af00dd46995408c4f8e12ca213b4
davipaes <davipaesoliveira@hotmail.com>
2024-05-04 09:37:17 -0300

    04/05

--- Assets/scripts/cameraseguir.cs
+++ Assets/scripts/cameraseguir.cs
@@ -1,9 +1,14 @@
+using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SeguirJogador : MonoBehaviour
 {
     public Transform Player; 
-    public float suavizacao = 5f; 
+    public float suavizacao = 5f;
+    public Transform alvo;
+    RaycastHit hit = new RaycastHit();
+    public float ajusteCamera;
 
     private void FixedUpdate()
     {
@@ -12,8 +17,20 @@ public class SeguirJogador : MonoBehaviour
             
             Vector3 followP = new Vector3(Player.position.x, Player.position.y-2, transform.position.z);
 
-            
+
+
+            if (Physics.Linecast(followP,transform.position, out hit))
+            {
+              transform.position = transform.position;
+               // transform.position = hit.point + transform.forward * ajusteCamera;
+                Debug.Log(hit.point + transform.forward * ajusteCamera);
+            }
+            else
+            {
             transform.position = Vector3.Lerp(transform.position, followP, suavizacao * Time.fixedDeltaTime);
+            }
+
+
         }
     }
 }
